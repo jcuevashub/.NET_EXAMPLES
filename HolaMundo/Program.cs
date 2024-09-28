48428b64714a358c14398ccea2e43b5819b7a987
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-28 07:46:21 -0400

    Variables y Tipos de Datos

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -1,7 +1,14 @@
-﻿class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hola Mundo desde .NET!");
-    }
-}
+﻿//Hello World
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.WriteLine("Hola Mundo desde .NET!");
+//     }
+// }
+
+//Variables y Tipos de Datos
+// int edad = 25;
+// string nombre = "Juan";
+// bool esEstudiante = true;
+// Console.WriteLine($"Hola, mi nombre es {nombre}, tengo {edad} años y ¿soy estudiante? {esEstudiante}");
