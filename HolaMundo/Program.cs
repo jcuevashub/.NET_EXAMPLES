ffa23147aaf65823572bc474ecb35b44c2f0f5f9
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-28 07:47:21 -0400

    Operadores y Expresiones

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -12,3 +12,15 @@
 // string nombre = "Juan";
 // bool esEstudiante = true;
 // Console.WriteLine($"Hola, mi nombre es {nombre}, tengo {edad} años y ¿soy estudiante? {esEstudiante}");
+
+
+//Operadores y Expresiones
+
+int a = 10;
+int b = 11;
+
+Console.WriteLine($"Suma: {a + b}");
+Console.WriteLine($"Resta: {a - b}");
+Console.WriteLine($"Multiplicación: {a * b}");
+Console.WriteLine($"División: {a / b}");
+Console.WriteLine($"¿a es mayor que b? {a > b}");
