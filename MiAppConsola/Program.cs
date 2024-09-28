1ddbd62f53e2f8b58b10b4681ec25f649ba96a23
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-28 16:33:24 -0400

    Lectura de Archivos

--- MiAppConsola/Program.cs
+++ MiAppConsola/Program.cs
@@ -26,21 +26,43 @@
 //----------------------------------------------------------------------------
 // Parámetros de Línea de Comandos
 // Puedes pasar argumentos a la aplicación mediante la línea de comandos. Estos se capturan en el array args del método Main.
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         if (args.Length == 0)
+//         {
+//             Console.WriteLine("No se pasaron argumentos.");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Argumentos recibidos: ");
+//             foreach(string arg in args)
+//             {
+//                 Console.WriteLine(arg);
+//             }
+//         }
+//     }
+// }
+
+// Lectura y Escritura de Archivos en Aplicaciones de Consola
+// En muchas aplicaciones de consola, es común interactuar con archivos. Puedes leer y escribir archivos utilizando clases como File y StreamReader/StreamWriter.
+
+//Lectura de Archivos
 class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length == 0)
+        string ruta = "archivo.txt";
+
+        if(File.Exists(ruta))
         {
-            Console.WriteLine("No se pasaron argumentos.");
+            string contenido = File.ReadAllText(ruta);
+            Console.WriteLine($"Contenido del archivo: {contenido}");
         }
-        else
-        {
-            Console.WriteLine("Argumentos recibidos: ");
-            foreach(string arg in args)
-            {
-                Console.WriteLine(arg);
-            }
-        }
+         else
+         {
+            Console.WriteLine("El archivo no existe.");
+         }
     }
 }
