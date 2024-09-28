c0443ac7e898e8d99bc0d08cf39198832b4226c5
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-28 16:09:23 -0400

    Encapsulamiento, Herencia y Polimorfismo

--- POO/Program.cs
+++ POO/Program.cs
@@ -1,14 +1,50 @@
 ﻿//Clases y Objetos
 //Una clase es una plantilla para cera objetos. Los objetos son instancias de una clase.
 
-public class Persona
-{
-    public string? Nombre { get; set; }
-    public int? Edad { get; set; }
+// public class Persona
+// {
+//     public string? Nombre { get; set; }
+//     public int? Edad { get; set; }
 
-    public void Presentarse()
+//     public void Presentarse()
+//     {
+//         Console.WriteLine($"Hola, mi nombre es {Nombre} y tengo {Edad} años.");
+//     }
+// }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+
+//         Persona persona = new Persona();
+//         persona.Nombre = "Carlos";
+//         persona.Edad = 30;
+
+//         persona.Presentarse();
+//     }
+// }
+
+//Encapsulamiento, Herencia y Polimorfismo
+// Encapsulamiento: Control del acceso a los miemros de una clase.
+// Herencia: Permite que una clase herede de otra.
+// Polimorfismo: Los métodos pueden comportarse de manera diferente según el contexto.
+
+class Animal 
+{
+    public string Nombre { get; set; }
+
+    public void Commer()
     {
-        Console.WriteLine($"Hola, mi nombre es {Nombre} y tengo {Edad} años.");
+        Console.WriteLine($"{Nombre} está comiendo.");
+    }
+}
+
+class Perro : Animal
+{
+    public void Ladrar()
+    {
+        Console.WriteLine($"{Nombre} está ladrando.");
     }
 }
 
@@ -16,11 +52,9 @@ class Program
 {
     static void Main(string[] args)
     {
-
-        Persona persona = new Persona();
-        persona.Nombre = "Carlos";
-        persona.Edad = 30;
-
-        persona.Presentarse();
+        Perro perro = new Perro();
+        perro.Nombre = "Max";
+        perro.Commer();
+        perro.Ladrar();
     }
 }
