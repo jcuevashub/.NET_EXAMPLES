6475cd7075ceb7b8d21773d5f0c9c55c6e5f3e6f
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-28 08:07:32 -0400

    Clases y Objetos

---
+++ POO/Program.cs
@@ -0,0 +1,26 @@
+﻿//Clases y Objetos
+//Una clase es una plantilla para cera objetos. Los objetos son instancias de una clase.
+
+public class Persona
+{
+    public string? Nombre { get; set; }
+    public int? Edad { get; set; }
+
+    public void Presentarse()
+    {
+        Console.WriteLine($"Hola, mi nombre es {Nombre} y tengo {Edad} años.");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Persona persona = new Persona();
+        persona.Nombre = "Carlos";
+        persona.Edad = 30;
+
+        persona.Presentarse();
+    }
+}
