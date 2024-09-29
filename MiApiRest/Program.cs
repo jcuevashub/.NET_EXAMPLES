92664c250bf9cf0a50c75d3827f378f87b2a9c93
Jackson Cuevas <jackson.cuevas@hotmail.com>
2024-09-28 21:11:47 -0400

    Monitorización y Logging Avanzado

--- MiApiRest/Program.cs
+++ MiApiRest/Program.cs
@@ -1,8 +1,16 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
